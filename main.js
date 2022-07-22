9fba047d897b3e9b8b6cb735a2e23fa64ab0e3b9
Daniel Tak <danieljtak@hotmail.com>
2022-07-22 23:30:52 +0000

    Week Planner in progress

--- main.js
+++ main.js
@@ -18,3 +18,24 @@ function submitForm(event) {
   $background.className = 'dark-background hidden';
   $entryModal.className = 'entry-modal hidden';
 }
+
+var $sunday = document.querySelector('.sunday');
+$sunday.addEventListener('click', openModal);
+
+var $monday = document.querySelector('.monday');
+$monday.addEventListener('click', openModal);
+
+var $tuesday = document.querySelector('.tuesday');
+$tuesday.addEventListener('click', openModal);
+
+var $wednesday = document.querySelector('.wednesday');
+$wednesday.addEventListener('click', openModal);
+
+var $thursday = document.querySelector('.thursday');
+$thursday.addEventListener('click', openModal);
+
+var $friday = document.querySelector('.friday');
+$friday.addEventListener('click', openModal);
+
+var $saturday = document.querySelector('.saturday');
+$saturday.addEventListener('click', openModal);
