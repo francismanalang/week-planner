7daffc2b9b2392cb6864dce7858fcb7f94913808
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-07-22 21:48:04 +0000

    still working

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-// var newVar = document.querySelector('.submit');
-// function handleSubmit(event) {
-//   newVar.className ='entry-modal'
-// }
+
+var $background = document.querySelector('.dark-background');
+var $entryModal = document.querySelector('.entry-modal');
+
+var $formEntry = document.querySelector('#form-entry');
+$formEntry.addEventListener('submit', submitForm);
+
+var $addEntry = document.querySelector('.add-entry');
+$addEntry.addEventListener('click', openModal);
+
+function openModal(event) {
+  $entryModal.className = 'entry-modal';
+  $background.className = 'dark-background';
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  $background.className = 'dark-background hidden';
+  $entryModal.className = 'entry-modal hidden';
+}
